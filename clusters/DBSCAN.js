827c593ad071b785a18f3ce815801492f8c46e1b
G3nKai <kulikovila2005@gmail.com>
2024-04-10 20:28:24 +0700

    не суть важно

--- clusters/DBSCAN.js
+++ clusters/DBSCAN.js
@@ -1,4 +1,7 @@
-//(DB, distFunc, Q, epsilon)
-function RangeQuery() {
-
+//(DB, distFunc, Q, epsilon) DB===points[] Q===dot  dist===euclidianDistance epsilon===задается на сайте пользователем
+function RangeQuery(dot) {
+    let neighbours = [];
+    for (let i = 0, len = points.length; i < len; i++) {
+        
+    }
 }
