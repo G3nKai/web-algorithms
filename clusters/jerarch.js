e5253fdba60d9fbc70fa1e92b1a3ef32f72d7b70
G3nKai <164293141+G3nKai@users.noreply.github.com>
2024-04-10 11:58:19 +0700

    Add files via upload

---
+++ clusters/jerarch.js
@@ -0,0 +1,64 @@
+function jerach() {
+    let clusters = [];
+    let centers = []; //изначальные центры
+
+    for (let i = 0; i < points.length; i++) {
+        centers.push(points[i]); // сколько точек - столько и центров кластеров. Изначально.
+        clusters.push([points[i]]);
+    }
+
+    while (clusters.length > k) {
+        let minDistance = Infinity;
+        let mergeIndices = [-1, -1];
+        
+        for (let i = 0; i < clusters.length - 1; i++) {
+            for (let j = i + 1; j < clusters.length; j++) {
+                const dist = euclideanDistance(centers[i], centers[j]);
+                if (dist < minDistance) {
+                    minDistance = dist;
+                    mergeIndices = [i, j];
+                }
+            }
+        }
+
+        //объединение двух ближайших кластеров
+        const mergedCluster = clusters[mergeIndices[0]].concat(clusters[mergeIndices[1]]);
+        const newCenter = calculateCenter(mergedCluster);
+        
+        clusters.splice(mergeIndices[1], 1);
+        clusters[mergeIndices[0]] = mergedCluster;
+        centers.splice(mergeIndices[1], 1);
+        centers[mergeIndices[0]] = newCenter;
+    }
+    colorizeClusters(clusters);
+    
+    return clusters;
+}
+
+// функция для расчета центра кластера
+function calculateCenter(cluster) {
+    let sumX = 0;
+    let sumY = 0;
+    for (let i = 0; i < cluster.length; i++) {
+        sumX += cluster[i][0];
+        sumY += cluster[i][1];
+    }
+    const centerX = sumX / cluster.length;
+    const centerY = sumY / cluster.length;
+    return [centerX, centerY];
+}
+
+function colorizeClusters(clusters) {
+    for (let i = 0; i < clusters.length; i++) {
+        const clusterColor = colours[i];
+
+        for (let j = 0; j < clusters[i].length; j++) {
+            const point = clusters[i][j];
+            const x = point[0];
+            const y = point[1];
+            
+            ctx.fillStyle = clusterColor;
+            ctx.fillRect(x, y, pointSize, pointSize);
+        }
+    }
+}
---
+++ clusters/test.js
@@ -0,0 +1,170 @@
+
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+const color = 'red';
+
+var pointSize = 5;//размер точки
+
+function drawPoint(x, y) {
+    ctx.fillStyle = color;
+    ctx.fillRect(x, y, pointSize, pointSize);
+    points.push([x, y]);
+}
+
+function clearCanvas() {
+    ctx.clearRect(0, 0, canvas.clientWidth, canvas.height);   
+    points = [];
+}
+
+canvas.addEventListener('click', function(event) {
+    let x = event.offsetX;
+    let y = event.offsetY;
+    drawPoint(x, y, color, pointSize);
+});
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//К-средние
+
+var points = [];//координаты ОТМЕЧЕННЫХ точек
+const colours = ['blue', 'brown', 'green', 'black', 'gray',
+                 'purple', 'yellow', 'orange'];
+let iterations = 50;//итерации
+
+var kk = document.getElementById("k");//кластеры
+var selectedValue = document.getElementById("selectedValue");
+
+var k = kk.value;
+selectedValue.textContent = k;
+
+kk.addEventListener("input", function() {
+    // Обновление значения переменной и его отображение на странице
+    k = this.value;
+    selectedValue.textContent = k;
+});
+
+function euclideanDistance(point1, point2) {
+    return (Math.pow(point2[0] - point1[0], 2) + Math.pow(point2[1] - point1[1], 2));
+}
+
+function randomIntFromInterval(min, max) { // min and max included
+    return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+function centroids() {
+    let dict = [];
+    let centers = [];
+    for (let i = 0; i < k;) {
+        let num = points[randomIntFromInterval(0, points.length - 1)];
+        if (!centers.includes(num)) {
+            centers[i] = num;
+            dict[i] = centers[i];
+            i++;
+        }
+    }
+    return centers;
+}
+
+function calculateWCSS(arr, centre) {
+    let wcss = 0;
+    for (let i = 0; i < arr.length; i++) {
+        let point = arr[i][0];
+        let clusterColor = arr[i][1];
+        let clusterIndex = colours.indexOf(clusterColor);
+        let centroid = centre[clusterIndex];
+        let distance = euclideanDistance(point, centroid);
+        wcss += Math.pow(distance, 2);
+    }
+    return wcss;
+}
+
+
+function kmeans() {
+    //ПЕРВОНАЧАЛЬНОЕ РАСПРЕДЕЛЕНИЕ КЛАСТЕРОВ
+    let centers = [];//координаты центров
+    centers = centroids();
+    ctx.clearRect(0, 0, canvas.clientWidth, canvas.height);   
+
+    let arr = [];
+
+    for (let i = 0; i < points.length; i++) {
+        let dist = [];
+
+        for (let j = 0; j < k; j++) {
+            dist[j] = euclideanDistance(points[i], centers[j]);
+        }
+        arr[i] = ([points[i], colours[dist.indexOf(Math.min(...dist))]]);
+        ctx.fillStyle = arr[i][1];
+        ctx.fillRect(arr[i][0][0], arr[i][0][1], pointSize, pointSize);
+    }
+    console.log("ПЕРВОНАЧАЛЬНО:", arr, centers);
+
+    ////////////////////////ПЕРЕСЧЕТ КЛАСТЕРОВ:
+    let centre = [];
+    let newCentroids = [];
+    let wcss = []
+    wcss[0] = [calculateWCSS(arr, centers), centers];
+    for (let num = 1; num < iterations; num++) {
+        newCentroids = [];
+
+        for (let i = 0; i < k; i++) {
+            newCentroids[i] = [];
+        }
+
+        for (let i = 0; i < arr.length; i++) {
+            newCentroids[colours.indexOf(arr[i][1])].push(arr[i][0]);
+        }
+
+        for (let i = 0; i < newCentroids.length; i++) {
+            let sumx = 0, sumy = 0;
+            for (let j = 0; j < newCentroids[i].length; j++) {
+                sumx += newCentroids[i][j][0];
+                sumy += newCentroids[i][j][1];
+            }
+            centre[i] = [sumx/newCentroids[i].length, sumy/newCentroids[i].length];
+        }
+
+        for (let i = 0; i < points.length; i++) {
+            let dist = [];
+    
+            for (let j = 0; j < k; j++) {
+                dist[j] = euclideanDistance(points[i], centre[j]);
+            }
+            arr[i] = ([points[i], colours[dist.indexOf(Math.min(...dist))]]);
+            ctx.fillStyle = arr[i][1];
+            ctx.fillRect(arr[i][0][0], arr[i][0][1], pointSize, pointSize);
+        }
+
+        if (wcss <= calculateWCSS(arr, centre)) {
+            for (let i = 0; i < k; i++) {
+                ctx.fillStyle = colours[i];
+                ctx.fillRect(centre[i][0], centre[i][1], 2, 2);
+            }
+            return;
+        }
+        wcss[num] = [calculateWCSS(arr, centre) - wcss[num - 1], centre];
+    }
+    console.log(wcss, wcss[wcss.indexOf(Math.min(...wcss))], (Math.min(...wcss)));
+
+    for (let final = 0; final < points.length; final++) {
+        let dist = [];
+
+        for (let ind = 0; ind < k; ind++) {
+            centers[ind] = wcss[wcss.indexOf(Math.min(...wcss))][1][ind];//индекс минимального отклонения
+        }
+
+        for (let prefinal = 0; prefinal < k; prefinal++) {
+            dist[prefinal] = euclideanDistance(points[i], centers[j]);
+        }
+        arr[final] = ([points[final], colours[dist.indexOf(Math.min(...dist))]]);
+        ctx.fillStyle = arr[final][1];
+        ctx.fillRect(arr[final][0][0], arr[final][0][1], pointSize, pointSize);
+    }
+
+    for (let i = 0; i < k; i++) {
+        ctx.fillStyle = colours[i];
+        ctx.fillRect(centre[i][0], centre[i][1], 2, 2);
+    }
+}
+
+
