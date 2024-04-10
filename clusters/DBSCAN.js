f65faee1e892513decc2a40ef837e7e5f52aead0
G3nKai <kulikovila2005@gmail.com>
2024-04-11 00:57:51 +0700

    первая часть функции

--- clusters/DBSCAN.js
+++ clusters/DBSCAN.js
@@ -25,6 +25,20 @@ preValueEps.addEventListener("input", function() {
 //сюда передаются var points, eps, minPTS 
 //в этой функции точки у которых точек >= minPTS принадлежат кластерам
 //если меньше, то просто шум (noise)
+
 function DBSCAN() {
-    
+    let visited = [];
+    let clusters = [];
+    let amount = 0;//счетчик кластеров
+    for (let i = 0, len = points.length; i < len; i++) {
+        visited[i] = points[i];
+        let dot = rangePoints(visited[i]);
+        if (dot >= minPTS) {
+            clusters[amount].concat(visited[i]);
+        }
+    }
+}
+
+function rangePoint(dot) {
+    //здесь я пытаюсь найти всех соседей dot в радиусе eps
 }
