9238423744eed609ec3af5f39d5af95b8bf0a229
G3nKai <kulikovila2005@gmail.com>
2024-04-10 22:58:15 +0700

    добавлено считывание элементов minPTS, eps.

--- clusters/DBSCAN.js
+++ clusters/DBSCAN.js
@@ -1,7 +1,30 @@
+var minPTSD = document.getElementById("PTS");
+var selectedPTS = document.getElementById("SelectedPTS");
+
+var minPTS = minPTSD.value;
+selectedPTS.textContent = minPTS;
+
+minPTSD.addEventListener("input", function() {
+    minPTS = this.value;
+    selectedPTS.textContent = minPTS;
+});
+
+
+var preValueEps = document.getElementById("eps");
+var SelectedEps = document.getElementById("SelectedEps");
+
+var eps = preValueEps.value;
+SelectedEps.textContent = eps;
+
+preValueEps.addEventListener("input", function() {
+    eps = this.value;
+    SelectedEps.textContent = eps;
+});
+
 //(DB, distFunc, Q, epsilon) DB===points[] Q===dot  dist===euclidianDistance epsilon===задается на сайте пользователем
-function RangeQuery(dot) {
+function RangeQuery() {
     let neighbours = [];
     for (let i = 0, len = points.length; i < len; i++) {
-        
+
     }
 }
